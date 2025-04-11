1988bf5aafd06fef5f708a7f8f64e12c0095aeee
EmilMCG <emilmateoc@gmail.com>
2025-04-11 08:16:28 -0500

    Add files via upload

--- Main.java
+++ Main.java
@@ -2,9 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package listasfor;
-
-import java.util.ArrayList;
+package matrizactividad;
 
 /**
  *
@@ -17,23 +15,6 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-     int i;
-        ArrayList<Integer> lst = new ArrayList();
-     
-     lst.add(200);
-     lst.add(300);
-     lst.add(400);
-     lst.add(500);
-     
-     
-     for (i=0;  i< lst.size();i++){
-         
-         System.out.println("Los valores del array son: " +lst.get(i));
-         
-         System.out.println("Imprimiendo Lista despues de eliminacion" +lst.remove(i));
-         
-         System.out.println(lst);
-     }
     }
     
 }
