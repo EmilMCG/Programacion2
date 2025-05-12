70d81cec9166412f0d50e0a5da933f2607e93df4
EmilMCG <emilmateoc@gmail.com>
2025-05-12 08:01:26 -0500

    Add files via upload

--- Clase 1 C3/Main.java
+++ Clase 1 C3/Main.java
@@ -1,8 +1,12 @@
 
 package manejodearchivo2;
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -20,7 +24,7 @@ public class Main {
         "Castro"
         );
         
-        try(Bufferedwriter = new BufferedWriter(new Filewriter("archivo.txt"))){
+        try(BufferedWriter writer = new BufferedWriter(new FileWriter("archivo.txt"))){
             for (String linea : lineas){
                 writer.write(linea);
                 writer.newLine();
@@ -31,8 +35,29 @@ public class Main {
         }
     }
     
-    private static void LeerArchivoListas(){
+    private static void LeerArchivoListas() throws FileNotFoundException, IOException{
+        
+        List<String> lineas = new ArrayList<>();
+        
+        try(BufferedReader reader = new BufferedReader(new FileReader("Archivo.txt"))) {
+            
+            String linea;
+            
+            while((linea = reader.readLine())!= null){
+             lineas.add(linea);   
+            }  
+        }catch(IOException e){
+            System.out.println("Error al leer archivo" + e.getMessage());
         
     }
+        System.out.println("Contenido del archivo: ");
+        
+        
+        for(String linea : lineas){
+            System.out.println(linea);
+        }
+}
+     
     
 }
+
